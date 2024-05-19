95136ff5c06bb7a76a2de5e5d716cd9970e3590f
Igor <33967454+utilizable@users.noreply.github.com>
2024-05-19 02:20:20 +0200

    feat: added q35; fetching newer bios file

--- qemu/qemu-windows/spin-up.cmd
+++ qemu/qemu-windows/spin-up.cmd
@@ -4,95 +4,101 @@ setlocal EnableDelayedExpansion
 :: FETCHING - QEMU
 :: -------------------
 
+:: Set the URL and output path
 set URL=https://qemu.weilnetz.de/w64/2023/qemu-w64-setup-20231224.exe
 set QEMU_DOWNLOAD_PATH=%USERPROFILE%/Downloads/qemu-w64-setup-20231224.exe
-set QEMU_BINARY_PATH=%USERPROFILE%/qemu_core
+set QEMU_BINARY_PATH=%USERPROFILE%\qemu_core
 
-:: fetch iso 
-if not exist "%QEMU_DOWNLOAD_PATH%" (
-  curl -A "Wget" -o "%QEMU_DOWNLOAD_PATH%" "%URL%"
-)
-:: unzip iso
 if not exist "%QEMU_BINARY_PATH%" (
+
+  :: fetch iso 
+  if not exist "%QEMU_DOWNLOAD_PATH%" (
+    curl -A "Wget" -o "%QEMU_DOWNLOAD_PATH%" "%URL%"
+  )
+  :: unzip iso
   7z.exe x "%QEMU_DOWNLOAD_PATH%" -o%QEMU_BINARY_PATH% -y
 )
-
 :: MISCS
 :: -------------------
-:: we want to store re-usable assets inside our qemu binary directory
-
 set QEMU_MISC_PATH=%QEMU_BINARY_PATH%/misc
+set QEMU_DISK_PATH=%QEMU_BINARY_PATH%/disks/%~n0/
 
 :: create misc directory
 if not exist "%QEMU_MISC_PATH%" (
   mkdir "%QEMU_MISC_PATH%"
 )
 
-:: FETCHING - ISO
-:: -------------------
-
-SET ISO_PATH=%QEMU_MISC_PATH%/debian-12.4.0-amd64-netinst.iso
-SET URL=https://gemmei.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-12.4.0-amd64-netinst.iso
-
-if not exist "%ISO_PATH%" (
-  curl -A "Wget" -o %ISO_PATH% "%URL%"
-)
-
-:: FETCHING - BIOS
-:: -------------------
-:: https://github.com/BlankOn/ovmf-blobs/blob/master/bios64.bin
-
-SET BIOS_PATH=%QEMU_MISC_PATH%/bios64.bin
-SET URL=https://raw.githubusercontent.com/BlankOn/ovmf-blobs/c9379b95fc2b1bf3a8ed90de0f60bd4f0a8b258b/bios64.bin
-
-if not exist "%BIOS_PATH%" (
-  curl -A "Wget" -o %BIOS_PATH% "%URL%"
-)
-
-:: VIRTUAL-MACHINE
-:: -------------------
-
-:: use this script file name
-set QEMU_DISK_PATH=%QEMU_BINARY_PATH%/disks/%~n0/
-
 :: create disk directory
 if not exist "%QEMU_DISK_PATH%" (
   mkdir "%QEMU_DISK_PATH%"
 )
 
+:: FETCHING - ISO
+:: -------------------
+SET ISO_PATH=%QEMU_MISC_PATH%/debian-12.4.0-amd64-netinst.iso
+SET URL=https://chuangtzu.ftp.acc.umu.se/cdimage/weekly-builds/amd64/iso-cd/debian-testing-amd64-netinst.iso
+
+if not exist "%ISO_PATH%" (
+	curl -A "Wget" -o %ISO_PATH% "%URL%"
+)
+
+:: --------------------------------------
+
+:: FETCHING - BIOS
+:: -------------------
+:: https://github.com/BlankOn/ovmf-blobs/blob/master/bios64.bin
+
+SET BIOS_PATH=%QEMU_MISC_PATH%/OVMF.fd
+SET URL=https://github.com/kholia/OSX-KVM/raw/326053dd61f49375d5dfb28ee715d38b04b5cd8e/OVMF_CODE.fd
+
+if not exist "%BIOS_PATH%" (
+	curl -L "%URL%" -o %BIOS_PATH%
+)
+
+:: --------------------------------------
+
+:: VIRTUAL-MACHINE
+:: -------------------
+
+:: DISKS
+:: -------------------
+
 :: disk - a
 :: -------------------
 set QEMU_VM_DISK_NAME=disk-a
-set QEMU_VM_DISK_SIZE=15G
+set QEMU_VM_DISK_SIZE=150G
 set QEMU_VM_DISK_A_PATH=%QEMU_DISK_PATH%/%QEMU_VM_DISK_NAME%
 if not exist "%QEMU_VM_DISK_A_PATH%" (
   %QEMU_BINARY_PATH%/qemu-img create -f qcow2 %QEMU_VM_DISK_A_PATH% %QEMU_VM_DISK_SIZE%
 )
 :: disk - b
 :: -------------------
-set QEMU_VM_DISK_NAME=disk-b
-set QEMU_VM_DISK_SIZE=50G
-set QEMU_VM_DISK_B_PATH=%QEMU_DISK_PATH%/%QEMU_VM_DISK_NAME%
-if not exist "%QEMU_VM_DISK_B_PATH%" (
-  %QEMU_BINARY_PATH%/qemu-img create -f qcow2 %QEMU_VM_DISK_B_PATH% %QEMU_VM_DISK_SIZE%
-)
+:: set QEMU_VM_DISK_NAME=disk-b
+:: set QEMU_VM_DISK_SIZE=150G
+:: set QEMU_VM_DISK_B_PATH=%QEMU_DISK_PATH%/%QEMU_VM_DISK_NAME%
+:: if not exist "%QEMU_VM_DISK_B_PATH%" (
+::  %QEMU_BINARY_PATH%/qemu-img create -f qcow2 %QEMU_VM_DISK_B_PATH% %QEMU_VM_DISK_SIZE%
+::)
 
-:: instance
+:: INSTANCE
 :: -------------------
 
-set QEMU_VM_SSH_PORT=8022
+set QEMU_VM_PORT_SSH=7000
+set QEMU_VM_PORT_HTTP_A=80
+set QEMU_VM_PORT_HTTP_B=6445
+set QEMU_VM_PORT_HTTP_C=443
 
 SET INVOKE=^
   %QEMU_BINARY_PATH%/qemu-system-x86_64.exe ^
   -accel whpx,kernel-irqchip=off ^
-  -m 2048 ^
+  -bios %BIOS_PATH% ^
+  -machine q35 ^
+  -m 8192 ^
   -boot d ^
-  -smp 2 ^
+  -smp 4 ^
+  -cdrom %ISO_PATH% ^
   -net nic,model=virtio ^
   -hda %QEMU_VM_DISK_A_PATH% ^
-  -hdb %QEMU_VM_DISK_B_PATH% ^
-  -cdrom %ISO_PATH% ^
-  -bios %BIOS_PATH% ^
-  -net user,hostfwd=tcp::%QEMU_VM_SSH_PORT%-:22
-  
+  -net user,hostfwd=tcp::%QEMU_VM_PORT_SSH%-:22,hostfwd=tcp::%QEMU_VM_PORT_HTTP_A%-:80,hostfwd=tcp::%QEMU_VM_PORT_HTTP_B%-:6445,hostfwd=tcp::%QEMU_VM_PORT_HTTP_C%-:443
+
 %INVOKE%
